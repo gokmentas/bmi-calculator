f795c9f6951c823634083cf37be43e85ad21d89c
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-03-14 00:13:09 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _InputPageState extends State<InputPage> {
         title: const Text("BMI CALCULATOR"),
       ),
       body: const Center(
-        child: Text("Body Tessxt"),
+        child: Text("Body Text"),
       ),
       floatingActionButton: FloatingActionButton(
         // shape: const CircleBorder(),
