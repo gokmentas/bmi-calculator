71711057e6988f13fc17fc7067a4aa13b9accf0f
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-03-14 01:34:48 +0300

    Seperated home page and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(const BMICalculator());
@@ -10,38 +11,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-      //   useMaterial3: true,
-      // ),
-      home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("BMI CALCULATOR"),
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: ThemeData.dark().appBarTheme.copyWith(
+              backgroundColor: const Color(0xFF0A0D22),
+            ),
+        scaffoldBackgroundColor: const Color(0xFF0A0D22),
+        // 0xFF111428
       ),
-      body: const Center(
-        child: Text("Body Text"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // shape: const CircleBorder(),
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      home: const InputPage(),
     );
   }
 }
