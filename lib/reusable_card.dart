6feb8e2dec404dedf8ea226ba05a9fbe3c8f5110
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-03-27 16:01:48 +0300

    Refactor GestureDetector

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget? cardChild;
-  const ReusableCard({super.key, required this.colour, this.cardChild});
+  final VoidCallback? onPress;
+  const ReusableCard({super.key, required this.colour, this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        // 0xFF1D1F33
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          // 0xFF1D1F33
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
